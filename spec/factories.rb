c80e5ed50172d70531c73d292cfe25f58de4f043
Andrei Tanana <tanana@email.ru>
2015-04-19 13:18:57 +0300

    product type model

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,8 @@ FactoryGirl.define do
   factory :category do
     name 'test category'
   end
+
+  factory :product_type do
+    name 'test product type'
+  end
 end
---
+++ spec/models/product_type_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe ProductType, type: :model do
+  it { is_expected.to respond_to(:name) }
+
+  context 'with name' do
+    let(:product_type) { FactoryGirl.build :product_type }
+
+    it 'is valid' do
+      expect(product_type).to be_valid
+    end
+
+    context 'with not unique name' do
+      let(:product_type2) { FactoryGirl.build :product_type }
+
+      it 'is not valid' do
+        product_type2.save
+        expect(product_type).to_not be_valid
+      end
+    end
+  end
+
+  context 'without name' do
+    let(:product_type) { FactoryGirl.build :product_type, name: '' }
+
+    it 'is not valid' do
+      expect(product_type).to_not be_valid
+    end
+  end
+end
