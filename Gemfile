f5a7f206510933d135099e84e25d201ac7badaca
Andrei Tanana <tanana@email.ru>
2015-04-19 13:13:13 +0300

    category model

--- Gemfile
+++ Gemfile
@@ -47,5 +47,6 @@ group :development, :test do
   gem 'spring'
 
   gem 'rspec-rails'
+  gem 'factory_girl_rails'
 end
 
---
+++ spec/models/category_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  it { is_expected.to respond_to(:name) }
+
+  context 'with name' do
+    let(:category) { FactoryGirl.build :category }
+
+    it 'is valid' do
+      expect(category).to be_valid
+    end
+
+    context 'with not unique name' do
+      let(:category2) { FactoryGirl.build :category }
+
+      it 'is not valid' do
+        category2.save
+        expect(category).to_not be_valid
+      end
+    end
+  end
+
+  context 'without name' do
+    let(:category) { FactoryGirl.build :category, name: '' }
+
+    it 'is not valid' do
+      expect(category).to_not be_valid
+    end
+  end
+end
